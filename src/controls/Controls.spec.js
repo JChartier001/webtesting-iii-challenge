7f04dc2de08422b249ea64a76b811944825d54de
JChartier001 <JChartier001@gmail.com>
2019-11-11 17:45:43 -0600

    tests passing except 1

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,63 @@
-// Test away!
+import React from "react"
+import Controls from "./Controls"
+import { render, fireEvent } from "@testing-library/react";
+
+
+
+
+test('Controls renders correctly', () => {
+    render(<Controls />);  
+})
+
+
+test('closed is called', () => {
+  const toggleClosed = jest.fn();
+  const { getByText } = render(
+      <Controls toggleClosed={toggleClosed} closed={false} />
+  )
+fireEvent.click(getByText(/Close gate/i));
+
+  expect(toggleClosed).toHaveBeenCalled();
+  expect(toggleClosed).toHaveBeenCalledTimes(1);
+});
+
+
+test('unlocked and open', () => {
+    const closeSpy = jest.fn();
+    const { getByText } = render(<Controls locked={false} closed={false} toggleClosed={closeSpy}/>);
+    const lockBtn = getByText(/lock gate/i);
+    const closeBtn = getByText(/close gate/i);
+    expect(lockBtn.disabled).toBe(true);
+    expect(closeBtn.disabled).toBe(false);
+
+    fireEvent.click(closeBtn);    
+    expect(closeSpy).toBeCalled();
+  });
+
+  test('unlocked and closed', () => {
+    const openSpy = jest.fn();
+    const lockSpy = jest.fn();
+    const { getByText } = render(<Controls locked={false} closed={true} toggleClosed={openSpy} toggleLocked={lockSpy}/>);
+    const lockBtn = getByText(/lock gate/i);
+    const openBtn = getByText(/open gate/i);
+    expect(lockBtn.disabled).toBe(false);
+    expect(openBtn.disabled).toBe(false);
+
+    fireEvent.click(openBtn);
+    expect(openSpy).toBeCalled();
+
+    fireEvent.click(lockBtn);
+    expect(lockSpy).toBeCalled();
+  });
+
+  test('locked and closed', () => {
+    const unlockSpy = jest.fn();
+    const { getByText } = render(<Controls locked={true} closed={true} toggleLocked={unlockSpy}/>);
+    const unlockBtn = getByText(/unlock gate/i);
+    const openBtn = getByText(/open gate/i);
+    expect(unlockBtn.disabled).toBe(false);
+    expect(openBtn.disabled).toBe(true);
+
+    fireEvent.click(unlockBtn);
+    expect(unlockSpy).toBeCalled();
+  });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,27 @@
-// Test away!
+import React from "react";
+import Display from "./Display"
+import { render } from "react-testing-library";
+
+test('unlocked and open', () => {
+    const { getByText } = render(<Display locked={false} closed={false}/>);
+    const unlocked = getByText(/unlocked/i);
+    const open = getByText(/open/i);
+    expect(unlocked.className).toMatch(/\bgreen-led\b/);
+    expect(open.className).toMatch(/\bgreen-led\b/);
+  });
+
+test('unlocked and closed', () => {
+    const { getByText } = render(<Display locked={false} closed={true}/>);
+    const unlocked = getByText(/unlocked/i);
+    const closed = getByText(/closed/i);
+    expect(unlocked.className).toMatch(/\bgreen-led\b/);
+    expect(closed.className).toMatch(/\bred-led\b/);
+  });
+
+test('locked and closed', () => {
+    const { getByText } = render(<Display locked={true} closed={true}/>);
+    const locked = getByText(/locked/i);
+    const closed = getByText(/closed/i);
+    expect(locked.className).toMatch(/\bred-led\b/);
+    expect(closed.className).toMatch(/\bred-led\b/);
+  });
